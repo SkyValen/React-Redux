39bca3fdcb4d94949bdccbdfe0765a21c03f7e4f
NikitaSkorohov <nikitaskoroho14@gmail.com>
2024-11-14 12:17:27 +0200

    Adding buttons to increase by 5 and decrease by 3

---
+++ src/actions.js
@@ -0,0 +1,5 @@
+export const inc = () => ({type: 'INC'});
+export const dec = () => ({type: 'DEC'});
+export const rnd = (value) => ({type: 'RND', payload: value});
+export const inc5 = () => ({type: 'INC5'});
+export const dec3 = () => ({type: 'DEC3'});
--- src/index.js
+++ src/index.js
@@ -1,50 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-
-const initialState = { value: 0 };
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "INC":
-      return {
-        ...state,
-        value: state.value + 1
-      };
-    case "DEC":
-      return {
-        ...state,
-        value: state.value - 1
-      };
-    case "RND":
-      return {
-        ...state,
-        value: state.value * action.payload
-      };
-    default:
-      return state;
-  }
-}
+import { createStore, bindActionCreators } from 'redux';
+import reducer from './reducer';
+import * as actions from './actions';
 
 const store = createStore(reducer);
 
+const {dispatch, subscribe, getState} = store
+
 const update = () => {
-  document.getElementById('counter').textContent = store.getState().value;
+  document.getElementById('counter').textContent = getState().value;
 }
 
-store.subscribe(update);
+subscribe(update);
 
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (value) => ({type: 'RND', payload: value});
+const {inc, dec, rnd, inc5, dec3} = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc())
-})
+document.getElementById('inc').addEventListener('click', inc)
 
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec())
-})
+document.getElementById('dec').addEventListener('click', dec)
 
 document.getElementById('rnd').addEventListener('click', () => {
   const random = Math.random() * 10
@@ -52,6 +26,9 @@ document.getElementById('rnd').addEventListener('click', () => {
   store.dispatch(rnd(value));
 });
 
+document.getElementById('inc5').addEventListener('click', inc5);
+
+document.getElementById('dec3').addEventListener('click', dec3);
 
 
 // let state = reducer(initialState, {type: 'INC'});
